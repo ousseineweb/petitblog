4a922eac22b5b4ad62d7e2c998622ea7eeb656e0
ousseineweb <ousseineweb@gmail.com>
2022-06-30 04:22:03 +0200

    configuration in app: navbar footer homepage and styles

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+// import dependencies
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min';
+
